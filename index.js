439e64bf8e6e75519da7f7875e41e4f3464c65fe
Roopal-Jain <roopal2601@gmail.com>
2022-02-08 23:28:56 +0530

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+var a;
+a="hello";
+console.log(a);
+console.log(typeof a);
+var str=`hello
+how are you`;
+console.log(str);
+var str1="hello\nhow are you";
+console.log(str1);
+var num=300;
+console.log(`half of ${num} is ${num/2}`);
